60f4794fb9b979cad56229ea407bb9cd95c7fc69
Yardena Meymann <yardenam@feature.fm>
2020-05-15 13:16:56 +0300

    Sketching: Beginning to work.

--- app/src/main/java/com/playground/myapplication/MainActivity.kt
+++ app/src/main/java/com/playground/myapplication/MainActivity.kt
@@ -4,31 +4,46 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
-    private var testPad: SketchPadManager? = null
+    private val testPad: SketchPadManager by lazy { SketchPadManager(testPadView) }
+    private val loadImageLiveData = MutableLiveData<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initPicturePad("IMG_6893.JPG")
+
+        clearHolesButton.setOnClickListener {
+            loadImageLiveData.postValue(
+                loadImageLiveData.value ?: "IMG_6893.JPG"
+            )
+        }
+        fillHolesButton.setOnClickListener {
+            // TODO: Fill the holes engine
+            // TODO: testPad?.load()
+        }
+
+        loadImageLiveData.observe(this, Observer { name ->
+            if (name != null) {
+                launchUI {
+                    val bitmap = loadBitmap(name)
+                    testPad.reload(bitmap)
+                }
+            }
+        })
+
+        loadImageLiveData.postValue("IMG_6893.JPG")
     }
 
     @Suppress("SameParameterValue")
-    private fun initPicturePad(pictureAsset: String) {
-        launchUI {
-            val bitmap = withBlocking {
-                val inputStream = assets.open(pictureAsset)
-                val immutable = BitmapFactory.decodeStream(inputStream)
-                // We don't scale down because the main goal of the test is to be applied on enormous bitmaps
-                immutable.copy(Bitmap.Config.ARGB_8888, true)
-            }
-
-            testPad = SketchPadManager(sketchPad, bitmap)
+    private suspend fun loadBitmap(pictureAsset: String): Bitmap {
+        return withBlocking {
+            val inputStream = assets.open(pictureAsset)
+            val immutable = BitmapFactory.decodeStream(inputStream)
+            // We don't scale down because the main goal of the test is to be applied on enormous bitmaps
+            immutable.copy(Bitmap.Config.ARGB_8888, true)
         }
     }
 
--- app/src/main/java/com/playground/myapplication/SketchPad.kt
+++ app/src/main/java/com/playground/myapplication/SketchPad.kt
@@ -6,24 +6,41 @@ import android.view.MotionEvent
 import android.widget.ImageView
 
 
-class SketchPadManager(private val imageView: ImageView, sourceBitmap: Bitmap) {
-    private val bitmap = Bitmap.createBitmap(sourceBitmap.width, sourceBitmap.height, Bitmap.Config.RGB_565)
-    private val canvas: Canvas = Canvas(bitmap)
+class SketchPadManager(private val imageView: ImageView) {
+    private lateinit var canvas: Canvas
+    private val dirtPaint: Paint = Paint()
     private var penPoint: PointF? = null
-    private val dirtPaint:Paint = Paint()
 
     init {
         dirtPaint.color = Color.RED
         dirtPaint.strokeCap = Paint.Cap.ROUND
         dirtPaint.strokeWidth = imageView.resources.getDimension(R.dimen.sketchPadBrushSize)
         bindClickListener(imageView)
+    }
 
+    /**
+     * Reload an image as GreyScale to the imageView
+     *
+     * **Note that this one is blocky** - Use it for test only
+     *
+     * @param sourceBitmap Source bitmap, can be in colour. It will be converted to greyscale.
+     *
+     */
+    // TODO: Split to main and async part.
+    fun reload(sourceBitmap: Bitmap) {
+        val bitmap =
+            Bitmap.createBitmap(sourceBitmap.width, sourceBitmap.height, Bitmap.Config.RGB_565)
+        canvas = Canvas(bitmap)
+        drawBitmapAsGreyScale(sourceBitmap)
+        imageView.setImageBitmap(bitmap)
+    }
+
+    private fun drawBitmapAsGreyScale(sourceBitmap: Bitmap) {
         val colorMatrix = ColorMatrix()
         colorMatrix.setSaturation(0f)
         val paint = Paint()
         paint.colorFilter = ColorMatrixColorFilter(colorMatrix)
         canvas.drawBitmap(sourceBitmap, 0f, 0f, paint)
-        imageView.setImageBitmap(bitmap)
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -40,9 +57,9 @@ class SketchPadManager(private val imageView: ImageView, sourceBitmap: Bitmap) {
      * @return true iff successful
      */
     private fun strokeToPoint(x: Float, y: Float) : Boolean {
-        val pen = penPoint?:return false
-        canvas.drawLine(pen.x, pen.y, x, y, dirtPaint)
-        penPoint = PointF(x, y)
+        val sourcePoint = penPoint ?: return false
+        val targetPoint = PointF(sourcePoint.x + x, sourcePoint.y + y).also { penPoint = it }
+        canvas.drawLine(sourcePoint.x, sourcePoint.y, targetPoint.x, targetPoint.y, dirtPaint)
         imageView.postInvalidate()
         return true
     }
@@ -59,6 +76,5 @@ class SketchPadManager(private val imageView: ImageView, sourceBitmap: Bitmap) {
             else -> false
         }
     }
-
     private fun setupClick(imageView: ImageView) = imageView
 }
